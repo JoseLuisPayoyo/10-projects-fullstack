06565637687b5fb1cb31f1c374a8f6c65e3b8814
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-30 19:04:13 +0100

    interfax de tarea service

--- 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/services/ITaskService.java
+++ 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/services/ITaskService.java
@@ -1,5 +1,10 @@
 package com.payoyo.task_tracker_backend.services;
 
+import java.util.List;
+
+import com.payoyo.task_tracker_backend.dto.task.TaskRequest;
+import com.payoyo.task_tracker_backend.dto.task.TaskResponse;
+
 /*
  * Servicio de gestion de tareas
  */
@@ -7,6 +12,35 @@ public interface ITaskService {
     
     /*
      * Crear nueva tarea
-     * @param Tarea
+     * @param TareaRequest datos de la tarea
+     * @return Tarea creada
      */
+    TaskResponse createTask(TaskRequest request);
+
+    /*
+     * Actualizar tarea
+     * @param TareaRequest datos de la tarea
+     * @param ID id de la tarea a actualizar
+     * @return Tarea actualizada
+     */
+    TaskResponse updateTask(Long id, TaskRequest request);
+
+    /*
+     * Eliminar tarea
+     * @param ID id de la tarea 
+     */
+    void deleteTask(Long id);
+
+    /*
+     * Listar todas las tareas
+     * @return Lista de las tareas
+     */
+    List<TaskResponse> getTasks();
+
+    /*
+     * Listar tarea por id
+     * @param ID id de la tarea a listar
+     * @return Tarea
+     */
+    TaskResponse getTaskById(Long id);
 }
---
+++ 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/services/impl/TaskServiceImpl.java
@@ -0,0 +1,51 @@
+package com.payoyo.task_tracker_backend.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.payoyo.task_tracker_backend.dto.task.TaskRequest;
+import com.payoyo.task_tracker_backend.dto.task.TaskResponse;
+import com.payoyo.task_tracker_backend.repositories.TaskRepository;
+import com.payoyo.task_tracker_backend.services.ITaskService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TaskServiceImpl implements ITaskService{
+
+    private final TaskRepository taskRepository;
+
+    @Override
+    @Transactional
+    public TaskResponse createTask(TaskRequest request) {
+        
+    }
+
+    @Override
+    public TaskResponse updateTask(Long id, TaskRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTask'");
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTask'");
+    }
+
+    @Override
+    public List<TaskResponse> getTasks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTasks'");
+    }
+
+    @Override
+    public TaskResponse getTaskById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTaskById'");
+    }
+    
+}
