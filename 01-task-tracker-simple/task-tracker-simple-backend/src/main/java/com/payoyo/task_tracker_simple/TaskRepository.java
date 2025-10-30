336be874f89325fb244eaf04f9b1711b30e76dce
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-30 12:27:23 +0100

    repo de task

--- 01-task-tracker-simple/task-tracker-simple-backend/src/main/java/com/payoyo/task_tracker_simple/TaskRepository.java
+++ 01-task-tracker-simple/task-tracker-simple-backend/src/main/java/com/payoyo/task_tracker_simple/TaskRepository.java
@@ -1,5 +1,8 @@
 package com.payoyo.task_tracker_simple;
 
-public class TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
     
 }
