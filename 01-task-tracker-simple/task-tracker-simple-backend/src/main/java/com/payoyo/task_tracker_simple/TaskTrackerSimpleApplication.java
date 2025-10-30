b4db517669bed2e32af2b6be0d2206cf0f43900c
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-30 11:59:52 +0100

    inicio backend

---
+++ 01-task-tracker-simple/task-tracker-simple-backend/src/main/java/com/payoyo/task_tracker_simple/TaskTrackerSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.payoyo.task_tracker_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerSimpleApplication.class, args);
+	}
+
+}
---
+++ 01-task-tracker-simple/task-tracker-simple-backend/src/test/java/com/payoyo/task_tracker_simple/TaskTrackerSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoyo.task_tracker_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
