d7c6c185617e1bdecf2949d2c35b16672b2273d1
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-31 09:22:30 +0100

    mapper de task

--- 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/util/TaskMapper.java
+++ 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/util/TaskMapper.java
@@ -1,22 +1,48 @@
 package com.payoyo.task_tracker_backend.util;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Locale.Category;
 
 import org.springframework.stereotype.Component;
 
 import com.payoyo.task_tracker_backend.dto.task.TaskRequest;
+import com.payoyo.task_tracker_backend.dto.task.TaskResponse;
+import com.payoyo.task_tracker_backend.models.Category;
 import com.payoyo.task_tracker_backend.models.Task;
 
 @Component
 public class TaskMapper {
-
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME; 
     
     /*
      * Convertir dto a entidad
      * @param dto
      * @return entidad
      */
+    public static Task toEntity(TaskRequest request, Category category) {
+        Task task = new Task();
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+        task.setPriority(request.getPriority());
+        task.setCategory(category);
+        task.setCompleted(false);
+
+        return task;
+    }
+
+    /*
+     * Convertir entidad a dto
+     * @param entity
+     * @return dto
+     */
+    public static TaskResponse toResponse(Task task) {
+        return TaskResponse.builder()
+            .id(task.getId())
+            .title(task.getTitle())
+            .description(task.getDescription())
+            .priority(task.getPriority() != null ? task.getPriority().name() : null)
+            .completed(task.isCompleted())
+            .createdAt(task.getCreatedAt())
+            .categoryId(task.getCategory().getId())
+            .build();
+    }
     
 }
