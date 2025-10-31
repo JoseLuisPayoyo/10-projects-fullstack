db6367e5598c519642a09237f00cd208d1f686e4
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-10-31 09:24:34 +0100

    categoria mapper

---
+++ 02-task-tracker/task-tracker-backend/src/main/java/com/payoyo/task_tracker_backend/util/CategoryMapper.java
@@ -0,0 +1,36 @@
+package com.payoyo.task_tracker_backend.util;
+
+import org.springframework.stereotype.Component;
+
+import com.payoyo.task_tracker_backend.dto.category.CategoryRequest;
+import com.payoyo.task_tracker_backend.dto.category.CategoryResponse;
+import com.payoyo.task_tracker_backend.models.Category;
+
+@Component
+public class CategoryMapper {
+    
+    /*
+     * Convertir categoria a entidad
+     * @param dto
+     * @return entidad
+     */
+    public static Category toEntity(CategoryRequest dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+        category.setDescription(dto.getDescription());
+        return category;
+    }
+
+    /*
+     * Convertir entidad a categoria
+     * @param entidad
+     * @return dto
+     */
+    public static CategoryResponse toResponse(Category category) {
+        CategoryResponse dto = new CategoryResponse();
+        dto.setId(category.getId());
+        dto.setName(category.getName());
+        dto.setDescription(category.getDescription());
+        return dto;
+    }
+}
